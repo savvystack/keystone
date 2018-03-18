8818d7dd282bec13f2f4b428d6a3586bfd116511
Lijian Li <jim@savvystack.com>
2018-03-18 15:42:16 -0700

    /api/count: only count authorized documents for "organizers"

--- admin/server/api/counts.js
+++ admin/server/api/counts.js
@@ -4,7 +4,9 @@ module.exports = function (req, res) {
 	var keystone = req.keystone;
 	var counts = {};
 	async.each(keystone.lists, function (list, next) {
-		list.model.count(function (err, count) {
+		// Savvy Stack: If the list has field "organizers", and the user is organizer, only count authorized documents
+		var conditions = list.fields.organizers && req.user.isOrganizer ? { organizers: req.user._id } : {};
+		list.model.count(conditions, function (err, count) {
 			counts[list.key] = count;
 			next(err);
 		});
