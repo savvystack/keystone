19e3be0a5f6db6036e25c2cd8d00654320d76f53
Lijian Li <jim@savvystack.com>
2018-04-10 14:58:51 -0700

    Added request to query object, so that pre/post hooks can modify conditions based on URL

--- admin/server/api/download.js
+++ admin/server/api/download.js
@@ -78,6 +78,8 @@ module.exports = function (req, res) {
 	if (relFields) {
 		query.populate(relFields.join(' '));
 	}
+	// Savvy Stack: Add request object into query object, so that pre/post:find hooks can modify the query based on particular request
+	query._req = req;
 	query.exec(function (err, results) {
 
 		if (err) return res.status(500).json(err);
